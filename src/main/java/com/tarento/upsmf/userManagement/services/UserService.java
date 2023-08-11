6839142f6a9699ca6db4941811c27df25b7609d0
Mahesh Maney R <mahesh-maney@users.noreply.github.com>
2023-08-11 12:19:44 +0530

    Update UserService.java

--- src/main/java/com/tarento/upsmf/userManagement/services/UserService.java
+++ src/main/java/com/tarento/upsmf/userManagement/services/UserService.java
@@ -32,9 +32,10 @@ public class UserService {
         logger.info("Getting headers...");
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add("Authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJSR3RkMkZzeG1EMnJER3I4dkJHZ0N6MVhyalhZUzBSSyJ9.kMLn6177rvY53i0RAN3SPD5m3ctwaLb32pMYQ65nBdA");
-        headers.add("x-authenticated-user-token","eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJBbjFzUzNFanlFeUhYRGdlWXUxdGdpTjM2SDdyalFOcld0a3NOQzhnbUNVIn0.eyJqdGkiOiJmODNlZTgzOS05NmM0LTQ3NWMtYTk2Yi1lNDI2MDViYmNjMjMiLCJleHAiOjE2OTA4Mzk2OTgsIm5iZiI6MCwiaWF0IjoxNjkwNzk2NDk4LCJpc3MiOiJodHRwczovL3VwaHJoLmluL2F1dGgvcmVhbG1zL3N1bmJpcmQiLCJhdWQiOlsicmVhbG0tbWFuYWdlbWVudCIsImFjY291bnQiXSwic3ViIjoiYTk3YjQyY2UtNzkwNi00MTRlLWJjMzAtMzFhODk0NjgwZjUzIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoibG1zIiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiZjc2NjlhYmItOWE4Ni00MWNjLWI2OWUtM2Q4ZTZjZjE2MjI2IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL3VwaHJoLmluIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsImFkbWluIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJyZWFsbS1tYW5hZ2VtZW50Ijp7InJvbGVzIjpbIm1hbmFnZS11c2VycyIsInZpZXctdXNlcnMiLCJxdWVyeS1ncm91cHMiLCJxdWVyeS11c2VycyJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiIiLCJuYW1lIjoiYWRtaW4gYWRtaW4iLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJhZG1pbiIsImdpdmVuX25hbWUiOiJhZG1pbiIsImZhbWlseV9uYW1lIjoiYWRtaW4iLCJlbWFpbCI6ImFkbWluQHN1bmJpcmQub3JnIn0.fh9Zj8htV_-ecMWF_5E2zNsZTZCITP509uJk7zSm6o-uqLRLXy6YupbNbyUZmAL_CkkmAawCtQcNA4GTyaacSA4mRBDfWrYeLfrguFbmgTMMuLOLGzfCfnTwDYhAoyZlyO8P8pnw9B8a0nKClqbqt1h2kpGYDmNV7fXltrE4f81IDXGGIbxuWrFhqmqT2xXi4gIf6Y6ANXFoU4jYaqRXw3hoYwCyYDFi57Dljzx_KWfCb4UxKqmVF1vlLokNVoYuhMNZSICEwJCBIS_PrY9sfoYZZK1Q04NWs1F6Hppqy85PhWn_EycaEeZ0OAByIvi2HagdT0Ris2sHVFntphQbkA");
-        return headers;
+        String authToken = env.getProperty("AuthorizationToken");
+        String xAuthToken = env.getProperty("x-authenticated-user-token");
+        headers.add("Authorization","Bearer " + authToken);
+        headers.add("x-authenticated-user-token",xAuthToken);
     }
 
     public ResponseEntity<JsonNode> createUser(final JsonNode body) throws URISyntaxException {
@@ -91,37 +92,31 @@ public class UserService {
         return result;
     }
 
-    public ResponseEntity<JsonNode> sendOTP(int phoneNumber, String name, int otp) throws URISyntaxException {
-        logger.info("Sending OTP...");
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/json; charset=utf8");
-        headers.add("Accept", "application/json");
-
-        HttpClient httpClient = HttpClientBuilder.create().build();
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        restTemplate.setRequestFactory(requestFactory);
-
+    public ResponseEntity<String> sendOTP(String phoneNumber, String name, String otp) throws URISyntaxException {
+        logger.info("Sending OTP to name : {} with OTP {} ...", name, otp);
         String baseUrl = "";
-        String username = "";
-        String password = "";
-        String senderId = "";
+        String username = env.getProperty("userName");
+        String password = env.getProperty("password");
+        String senderId = env.getProperty("senderID");
         String message = "Hello %s, Your OTP is %s UPSMF, Lucknow";
         String message1 = String.format(message,name,otp);
-        String destMobileNo = "";
         String msgType = "TXT";
         String response = "Y";
-
-        String payload = "username=" + URLEncoder.encode(username, StandardCharsets.UTF_8)
-                + "&pass=" + URLEncoder.encode(password, StandardCharsets.UTF_8)
-                + "&senderid=" + URLEncoder.encode(senderId, StandardCharsets.UTF_8)
-                + "&message=" + URLEncoder.encode(message1, StandardCharsets.UTF_8)
-                + "&dest_mobileno=" + URLEncoder.encode(String.valueOf(phoneNumber), StandardCharsets.UTF_8)
-                + "&msgtype=" + URLEncoder.encode(msgType, StandardCharsets.UTF_8)
-                + "&response=" + URLEncoder.encode(response, StandardCharsets.UTF_8);
-
-        HttpEntity<String> httpEntity = new HttpEntity<>(payload, headers);
-        ResponseEntity<JsonNode> result = restTemplate.exchange(baseUrl, HttpMethod.GET, httpEntity, JsonNode.class);
+        
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl)
+                .queryParam("username", username)
+                .queryParam("pass", password)
+                .queryParam("senderid", senderId)
+                .queryParam("message", message1)
+                .queryParam("dest_mobileno", String.valueOf(phoneNumber))
+                .queryParam("msgtype", msgType)
+                .queryParam("response", response);
+        
+        URI uri = builder.build().encode().toUri();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", "application/json");
+        HttpEntity<?> httpEntity = new HttpEntity<>(headers);
+        ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, String.class);
         return result;
     }
 }
