1166b1f920490d4887d207e9b9517f910631d766
Mahesh Maney R <mahesh-maney@users.noreply.github.com>
2023-08-23 16:53:19 +0530

    fixing null pointer issue <ManeyMR>.

--- src/main/java/com/tarento/upsmf/userManagement/services/UserService.java
+++ src/main/java/com/tarento/upsmf/userManagement/services/UserService.java
@@ -41,6 +41,7 @@ public class UserService {
     @Autowired
     private PaymentService paymentService;
 
+    @Autowired
     private KeycloakUserCredentialPersister keycloakUserCredentialPersister;
 
     private static Environment environment;
